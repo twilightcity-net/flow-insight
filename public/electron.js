bca445988b8804955c6c23642f01f1aff9d71032
Kara Marie Rawson <kara@openmastery.org>
2017-09-29 04:35:14 -0400

    Semantic-UI integration And Better Loading (#1)
    * added semantic react packages, css, and removed old css files
    
    * fixed linking assets in react semantic, and removed favicon for web stuff
    
    * updated install and app names and organized main entry point code
    
    * created better event callback handlers
    
    * fixed broken application icon
    
    * fixed invalid css classname in app view
    
    * disable unwanted text selection and hand cursor for deaktop app
    
    * fade window content on show to prevent janky rendering
    
    * fixed the tray icon to show and hide application
    
    * incremented verson to v0.0.2 to prep for release
    
    * upgraded dependencies for release

--- public/electron.js
+++ public/electron.js
@@ -1,50 +1,214 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+/*
+ * Required Packages
+ */
 
+const { app, BrowserWindow, ipcMain, Tray } = require('electron');
+const autoUpdater = require('electron-updater').autoUpdater;
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
 const notifier = require('node-notifier');
 const logger = require("electron-log");
 
-let mainWindow;
+/*
+ * Global Constants
+ */
 
-const autoUpdater = require('electron-updater').autoUpdater;
+const assetsDirectory = path.join(__dirname, 'assets');
+const applicationIcon = assetsDirectory + '/icons/icon.ico';
+const trayIcon = assetsDirectory + '/icons/icon.png';
 
+/*
+ * Global Objects
+ */
+
+let tray = undefined;
+let window = undefined;
+
+/*
+ * Application Events
+ */
+
+app.on('ready', onAppReadyCb);
+app.on('window-all-closed', onAppWindowAllCloseCb);
+app.on('activate', onAppActivateCb);
+
+/*
+ * Event Callback Functions
+ */
+
+function onAppReadyCb() {
+  createTray();
+  createWindow();
+}
+
+function onAppWindowAllCloseCb() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+}
+
+function onAppActivateCb() {
+  createWindow();
+}
+
+function onWindowReadyToShowCb() {
+  showWindow();
+}
+
+function onWindowCloseCb() {
+  window = null;
+}
+
+function onTrayRightClickCb() {
+  toggleWindow();
+}
+
+function onTrayDoubleClickCb() {
+  toggleWindow();
+}
+
+function onTrayClickCb(event) {
+    toggleWindow()
+
+    // Show devtools when command clicked
+    if (window.isVisible() && process.defaultApp && event.ctrlKey) {
+      showDevTools();
+    }
+}
+
+/*
+ * Creates the system tray object and icon. Called by onAppReadyCb()
+ */
+function createTray() {
+  tray = new Tray(trayIcon);
+  tray.on('right-click', onTrayRightClickCb);
+  tray.on('double-click', onTrayDoubleClickCb);
+  tray.on('click', onTrayClickCb);
+}
+
+/*
+ * Creates the main application window. Called by onAppReadyCb()
+ */
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(
+
+  // dont make the window if it already exists
+  if(window !== (null || undefined)) {
+    return;
+  }
+
+  // make new browser window and load view
+  window = new BrowserWindow(
+    {
+      name: 'metaos-console-window',
+      width: 900, 
+      height: 680,
+      show: false,
+      backgroundColor: '#ffffff',
+      icon: applicationIcon,
+      fullscreenable : false,
+      webPreferences: 
+      {
+        devTools: isDev,
+        toolbar: false
+      }
+    }
+  );
+  window.loadURL(
     isDev
       ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
-  mainWindow.on('closed', () => (mainWindow = null));
+  window.setMenu(null);
+
+  showDevTools();
+
+  // handle our windows events
+  window.on('ready-to-show', onWindowReadyToShowCb);
+  window.on('closed', onWindowCloseCb);
+
+  // start the autoupdate service
   initAutoUpdate();
 }
 
+/*
+ * show the main application window
+ */
+function showWindow() {
+  window.show();
+  window.focus();
+}
+
+/*
+ * hides the main application window
+ */
+function hideWindow() {
+  window.hide();
+}
+
+/*
+ * toggles window display between show and hide state
+ */
+function toggleWindow() {
+  if (window.isVisible()) {
+    hideWindow();
+  } else {
+    showWindow();
+  }
+}
+
+/*
+ * show the debug dev tools
+ */
+function showDevTools() {
+  if(!isDev) {
+    return
+  }
+  window.openDevTools(
+    {
+      detach: true
+    }
+  );
+}
+
+/*
+ * setup auto-update and check for updates. Called from createWindow()
+*/
 function initAutoUpdate() {
+
+  // skip update if we are in linux or dev mode
   if (isDev) {
     return;
   }
-
   if (process.platform === 'linux') {
     return;
   }
 
+  // configure update logging to file
   autoUpdater.logger = logger;
-  autoUpdater.logger.transports.file.level = "debug";
+  autoUpdater.logger.transports.file.level = 'info';
 
+  // check for updates and notify if we have a new version
   autoUpdater.checkForUpdates();
   autoUpdater.signals.updateDownloaded(showUpdateNotification);
 }
 
+/*
+ * shows system notification for when there is a new app update
+ * >> CURRENTLY BROKEN : not showing for when there is an update <<
+*/
 function showUpdateNotification(it) {
+  
+  // TODO implement logger into notifications
+
+  // define our object and lables
   it = it || {};
   const restartNowAction = 'Restart now';
+  const versionLabel = it.label 
+    ? `Version ${it.version}` 
+    : 'The latest version';
 
-  const versionLabel = it.label ? `Version ${it.version}` : 'The latest version';
-
+  // display a system notification dialog
   notifier.notify(
     {
       title: 'A new update is ready to install.',
@@ -61,17 +225,3 @@ function showUpdateNotification(it) {
     }
   );
 }
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
