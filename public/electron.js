8c52a8ca421126200a6f0196b850c32fa9bfbc28
Don Herkimer <donald.herkimer@yahoo.com>
2017-10-06 22:13:00 -0400

    https://www.pivotaltracker.com/story/show/151581762 - WIP Tweaked the menu creation to include default menus for Windows/Linux; add some EsLint globals

--- public/electron.js
+++ public/electron.js
@@ -42,8 +42,10 @@ app.on("window-all-closed", onAppWindowAllCloseCb);
 function onAppReadyCb() {
     app.setName('MetaOS');
     // createTray();
-  WindowManager.createWindowLoading();
-  // initAutoUpdate();
+    createMenu();
+    WindowManager.createWindowLoading();
+    WindowManager.createWindowBugReport();
+    // initAutoUpdate();
 }
 
 // FIXME doesn't work, untested
@@ -70,22 +72,21 @@ function onTrayClickCb(event) {}
 function createMenu() {
     let menu = null;
     if (process.platform === 'darwin') {
-        const template = [];
-        template.push({
-            label: app.getName(),
-            submenu: [
-                {role: 'about'},
-                {type: 'separator'},
-                {role: 'services', submenu: []},
-                {type: 'separator'},
-                {role: 'hide'},
-                {role: 'hideothers'},
-                {role: 'unhide'},
-                {type: 'separator'},
-                {role: 'quit'}
-            ]
-        })
-        template.push(
+        const template = [
+            {
+                label: app.getName(),
+                submenu: [
+                    {role: 'about'},
+                    {type: 'separator'},
+                    {role: 'services', submenu: []},
+                    {type: 'separator'},
+                    {role: 'hide'},
+                    {role: 'hideothers'},
+                    {role: 'unhide'},
+                    {type: 'separator'},
+                    {role: 'quit'}
+                ]
+            },
             {
                 role: 'window',
                 submenu: [
@@ -95,9 +96,7 @@ function createMenu() {
                     {type: 'separator'},
                     {role: 'front'}
                 ]
-            }
-        )
-        template.push(
+            },
             {
                 role: 'help',
                 submenu: [
@@ -110,15 +109,72 @@ function createMenu() {
                     {
                         label: 'Report bug',
                         click() {
+                            WindowManager.loadWindow(window)
                             createBugReportWindow();
                             showBugReportWindow();
                         }
                     }
-
                 ]
             }
-        )
-
+        ]
+        menu = Menu.buildFromTemplate(template)
+    } else {
+        const template = [
+            {
+                label: 'Edit',
+                submenu: [
+                    {role: 'undo'},
+                    {role: 'redo'},
+                    {type: 'separator'},
+                    {role: 'cut'},
+                    {role: 'copy'},
+                    {role: 'paste'},
+                    {role: 'pasteandmatchstyle'},
+                    {role: 'delete'},
+                    {role: 'selectall'}
+                ]
+            },
+            {
+                label: 'View',
+                submenu: [
+                    {role: 'reload'},
+                    {role: 'forcereload'},
+                    {role: 'toggledevtools'},
+                    {type: 'separator'},
+                    {role: 'resetzoom'},
+                    {role: 'zoomin'},
+                    {role: 'zoomout'},
+                    {type: 'separator'},
+                    {role: 'togglefullscreen'}
+                ]
+            },
+            {
+                role: 'window',
+                submenu: [
+                    {role: 'minimize'},
+                    {role: 'close'}
+                ]
+            },
+            {
+                role: 'help',
+                submenu: [
+                    {
+                        label: 'Learn More',
+                        click() {
+                            require('electron').shell.openExternal('http://www.openmastery.org/')
+                        }
+                    },
+                    {
+                        label: 'Report bug',
+                        click() {
+                            WindowManager.loadWindow(window)
+                            createBugReportWindow();
+                            showBugReportWindow();
+                        }
+                    }
+                ]
+            }
+        ]
         menu = Menu.buildFromTemplate(template)
     }
 
@@ -129,10 +185,10 @@ function createMenu() {
  * Creates the system tray object and icon. Called by onAppReadyCb()
  */
 function createTray() {
-  tray = new Tray(trayIcon);
-  tray.on("right-click", onTrayRightClickCb);
-  tray.on("double-click", onTrayDoubleClickCb);
-  tray.on("click", onTrayClickCb);
+    tray = new Tray(trayIcon);
+    tray.on("right-click", onTrayRightClickCb);
+    tray.on("double-click", onTrayDoubleClickCb);
+    tray.on("click", onTrayClickCb);
 }
 
 /*
@@ -140,6 +196,7 @@ function createTray() {
 */
 // TODO move to its own AppUpdater Class
 function initAutoUpdate() {
+
   // skip update if we are in linux or dev mode
   if (isDev) {
     return;
@@ -148,35 +205,35 @@ function initAutoUpdate() {
     return;
   }
 
-  autoUpdater.autoDownload = false;
+    autoUpdater.autoDownload = false;
 
-  // configure update logging to file
-  autoUpdater.logger = logger;
-  autoUpdater.logger.transports.file.level = "info";
+    // configure update logging to file
+    autoUpdater.logger = logger;
+    autoUpdater.logger.transports.file.level = "info";
 
-  autoUpdater.on("checking-for-update", () => {
-    autoUpdater.logger.info("Checking for update...");
-  });
-  autoUpdater.on("update-available", info => {
-    autoUpdater.logger.info("Update available.");
-  });
-  autoUpdater.on("update-not-available", info => {
-    autoUpdater.logger.info("Update not available.");
-  });
-  autoUpdater.on("error", err => {
-    autoUpdater.logger.error("Error in auto-updater.");
-  });
-  autoUpdater.on("download-progress", progressObj => {
-    let logMsg = "Download speed: " + progressObj.bytesPerSecond;
-    logMsg = logMsg + " - Downloaded " + progressObj.percent + "%";
-    logMsg =
+    autoUpdater.on("checking-for-update", () => {
+        autoUpdater.logger.info("Checking for update...");
+    });
+    autoUpdater.on("update-available", info => {
+        autoUpdater.logger.info("Update available.");
+    });
+    autoUpdater.on("update-not-available", info => {
+        autoUpdater.logger.info("Update not available.");
+    });
+    autoUpdater.on("error", err => {
+        autoUpdater.logger.error("Error in auto-updater.");
+    });
+    autoUpdater.on("download-progress", progressObj => {
+        let logMsg = "Download speed: " + progressObj.bytesPerSecond;
+        logMsg = logMsg + " - Downloaded " + progressObj.percent + "%";
+        logMsg =
       logMsg + " (" + progressObj.transferred + "/" + progressObj.total + ")";
-    autoUpdater.logger.info(logMsg);
-  });
-  autoUpdater.on("update-downloaded", info => {
-    autoUpdater.logger.info("Update downloaded");
-  });
+        autoUpdater.logger.info(logMsg);
+    });
+    autoUpdater.on("update-downloaded", info => {
+        autoUpdater.logger.info("Update downloaded");
+    });
 
-  // check for updates and notify if we have a new version
-  autoUpdater.checkForUpdates();
+    // check for updates and notify if we have a new version
+    autoUpdater.checkForUpdates();
 }
