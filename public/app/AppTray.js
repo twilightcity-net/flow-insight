d11bc9955256f988af1f43e884cbdb1e9551349b
Kara Marie Rawson <rawsonkara@gmail.com>
2017-12-05 07:42:34 -0500

    reverted theme merge, but kept fixed icons

--- public/app/AppTray.js
+++ public/app/AppTray.js
@@ -9,11 +9,11 @@ module.exports = class AppTray extends Tray {
   constructor() {
     log.info("[AppTray] created -> okay");
     let iconPath = Util.getAssetPath("/icons/icon.png");
-    if (process.platform === 'darwin') {
+    if (process.platform === "darwin") {
       // ??? Why didn't this work according to https://www.christianengvall.se/electron-app-icons/
       // iconPath = Util.getAssetPath("/icons/mac/icon.icns");
       iconPath = Util.getAssetPath("/icons/mac/png/icon.png");
-    } else if (process.platform !== 'win32') {
+    } else if (process.platform !== "win32") {
       iconPath = Util.getAssetPath("/icons/win/icon.ico");
     }
     log.info(`[AppTray] iconPath=${iconPath}`);
