d2597ff77a3859a3ccb1f3846839dc34f6f9d90a
Kara Marie Rawson <rawsonkara@gmail.com>
2017-12-06 09:45:22 -0500

    added more view components to the console

--- src/components/ConsoleTabs.js
+++ src/components/ConsoleTabs.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Menu, Icon, Segment } from "semantic-ui-react";
+import { Grid, Menu, Icon, Input, Segment } from "semantic-ui-react";
 
 /*
  * this component is the tab panel wrapper for the console content
@@ -21,6 +21,10 @@ export default class ConsoleTabs extends Component {
   }
 
   state = {};
+
+  handleRangeChange = e => {
+    console.log("[ConsoleTabs] range change -> " + e.target.value);
+  };
 
   handleMenuClick = (e, { name }) => {
     this.setState({ activeItem: name });
@@ -48,26 +52,59 @@ export default class ConsoleTabs extends Component {
         <div id="wrapper" className="content">
           <Segment.Group>
             <Segment inverted>
-              Ut vulputate sodales quam eu consequat. Vestibulum ante ipsum
-              primis in faucibus orci luctus et ultrices posuere cubilia Curae.
+              <Grid columns={16}>
+                <Grid.Column width={13} className="scrubber">
+                  <Input
+                    type="range"
+                    fluid
+                    max={256}
+                    onChange={this.handleRangeChange}
+                  />
+                </Grid.Column>
+                <Grid.Column width={3}>cell 2</Grid.Column>
+              </Grid>
             </Segment>
-            <Segment inverted>
-              Etiam non lectus in est finibus posuere. Fusce nisl lorem,
-              hendrerit eget lorem a
+          </Segment.Group>
+
+          <div className="content">
+            <Segment size="small" basic inverted>
+              Te eum doming eirmod, nominati pertinacia argumentum ad his.
             </Segment>
-            <Segment inverted>
-              Mauris laoreet scelerisque ipsum eget tristique.
+            <Segment size="small" basic inverted>
+              Pellentesque habitant morbi tristique senectus.
             </Segment>
+            <Segment size="small" basic inverted>
+              Eu quo homero blandit intellegebat. Incorrupte consequuntur mei
+              id.
+            </Segment>
+            <Segment size="small" basic inverted>
+              nominati quo argumentum tristique doming. homero senectus mei
+              Pellentesque.
+            </Segment>
+            <Segment size="small" basic inverted>
+              argumentum eirmod morbi Incorrupte intellegebat.
+            </Segment>
+            <Segment size="small" basic inverted>
+              Eu quo homero blandit intellegebat. Incorrupte consequuntur mei
+              id.
+            </Segment>
+          </div>
+
+          <Segment.Group>
             <Segment inverted>
-              Curabitur maximus enim vitae mauris ultrices, sed posuere ipsum
-              fringilla.
+              <Grid>
+                <Grid.Column width={4}>cell 1</Grid.Column>
+                <Grid.Column width={9}>cell 2</Grid.Column>
+                <Grid.Column width={3}>cell 3</Grid.Column>
+              </Grid>
             </Segment>
           </Segment.Group>
         </div>
         <div id="wrapper" className="menu">
-          <Menu inverted>
+          <Menu size="tiny" inverted>
             <Menu.Item
               name="journal"
+              color="violet"
               active={activeItem === "journal"}
               onClick={this.handleMenuClick}
             >
@@ -76,6 +113,7 @@ export default class ConsoleTabs extends Component {
             </Menu.Item>
             <Menu.Item
               name="torchie"
+              color="violet"
               active={activeItem === "torchie"}
               onClick={this.handleMenuClick}
             >
@@ -84,6 +122,7 @@ export default class ConsoleTabs extends Component {
             </Menu.Item>
             <Menu.Item
               name="flow"
+              color="violet"
               active={activeItem === "flow"}
               onClick={this.handleMenuClick}
             >
