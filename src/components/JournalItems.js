06429c77c335aae4177b53e7aef79fc2a572cf02
Kara Marie Rawson <rawsonkara@gmail.com>
2017-12-10 14:57:02 -0500

    added grid to journal items component and colored the task id

--- src/components/JournalItems.js
+++ src/components/JournalItems.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Segment } from "semantic-ui-react";
+import { Grid, Segment } from "semantic-ui-react";
 
 /*
  * this component is the tab panel wrapper for the console content
@@ -15,25 +15,82 @@ export default class JournalItems extends Component {
   render() {
     return (
       <div id="component" className="journalItems">
-        <Segment size="small" basic inverted>
-          Te eum doming eirmod, nominati pertinacia argumentum ad his.
-        </Segment>
-        <Segment size="small" basic inverted>
-          Pellentesque habitant morbi tristique senectus.
-        </Segment>
-        <Segment size="small" basic inverted>
-          Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id.
-        </Segment>
-        <Segment size="small" basic inverted>
-          nominati quo argumentum tristique doming. homero senectus mei
-          Pellentesque.
-        </Segment>
-        <Segment size="small" basic inverted>
-          argumentum eirmod morbi Incorrupte intellegebat.
-        </Segment>
-        <Segment size="small" basic inverted>
-          Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id.
-        </Segment>
+        <Grid inverted>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <div className="chunkTitle">torchie/US143242</div>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <div className="chunkText">
+                Te eum doming eirmod, nominati pertinacia argumentum ad his.
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <div className="chunkTitle">torchie/US157874</div>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <div className="chunkText">
+                Pellentesque habitant morbi tristique senectus.
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <div className="chunkTitle">torchie/US153345</div>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <div className="chunkText">
+                Eu quo homero blandit intellegebat. Incorrupte consequuntur mei
+                id.
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <div className="chunkTitle">torchie/US152224</div>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <div className="chunkText">
+                nominati quo argumentum tristique doming. homero senectus mei
+                Pellentesque.
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <div className="chunkTitle">torchie/US154445</div>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <div className="chunkText">
+                argumentum eirmod morbi Incorrupte intellegebat.
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <div className="chunkTitle">torchie/US233556</div>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <div className="chunkText">
+                Eu quo homero blandit intellegebat. Incorrupte consequuntur mei
+                id.
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <div className="chunkTitle">torchie/US233453</div>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <div className="chunkText">
+                Doming. homero senectus mei Pellentesque. Incorrupte
+                consequuntur mei id.
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
