973472c5699ea7778c3364b61414b5def99f45af
Kara Marie Rawson <rawsonkara@gmail.com>
2017-12-10 18:35:36 -0500

    created journal item component and added fade transition for hover over

---
+++ src/components/JournalItem.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Grid, Popup } from "semantic-ui-react";
+
+/*
+ * this component is the tab panel wrapper for the console content
+ */
+export default class JournalItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  /*
+   * renders the tab component of the console view
+   */
+  render() {
+    return (
+      <Grid.Row>
+        <Grid.Column width={2}>
+          <Popup
+            trigger={
+              <div className="chunkTitle">
+                {this.props.projectId} / {this.props.chunkId}
+              </div>
+            }
+            className="chunkTitle"
+            content={
+              <div>
+                <div>
+                  <i>{this.props.projectId}</i>
+                </div>
+                <div>
+                  <b>{this.props.chunkId}</b>
+                </div>
+              </div>
+            }
+            position="bottom left"
+            on="hover"
+            inverted
+          />
+        </Grid.Column>
+        <Grid.Column width={14}>
+          <div className="chunkText">{this.props.chunkText}</div>
+        </Grid.Column>
+      </Grid.Row>
+    );
+  }
+}
--- src/components/JournalItems.js
+++ src/components/JournalItems.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Grid, Segment } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
+import JournalItem from "./JournalItem";
 
 /*
  * this component is the tab panel wrapper for the console content
@@ -16,80 +17,41 @@ export default class JournalItems extends Component {
     return (
       <div id="component" className="journalItems">
         <Grid inverted>
-          <Grid.Row>
-            <Grid.Column width={2}>
-              <div className="chunkTitle">torchie/US143242</div>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <div className="chunkText">
-                Te eum doming eirmod, nominati pertinacia argumentum ad his.
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={2}>
-              <div className="chunkTitle">torchie/US157874</div>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <div className="chunkText">
-                Pellentesque habitant morbi tristique senectus.
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={2}>
-              <div className="chunkTitle">torchie/US153345</div>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <div className="chunkText">
-                Eu quo homero blandit intellegebat. Incorrupte consequuntur mei
-                id.
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={2}>
-              <div className="chunkTitle">torchie/US152224</div>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <div className="chunkText">
-                nominati quo argumentum tristique doming. homero senectus mei
-                Pellentesque.
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={2}>
-              <div className="chunkTitle">torchie/US154445</div>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <div className="chunkText">
-                argumentum eirmod morbi Incorrupte intellegebat.
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={2}>
-              <div className="chunkTitle">torchie/US233556</div>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <div className="chunkText">
-                Eu quo homero blandit intellegebat. Incorrupte consequuntur mei
-                id.
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={2}>
-              <div className="chunkTitle">torchie/US233453</div>
-            </Grid.Column>
-            <Grid.Column width={14}>
-              <div className="chunkText">
-                Doming. homero senectus mei Pellentesque. Incorrupte
-                consequuntur mei id.
-              </div>
-            </Grid.Column>
-          </Grid.Row>
+          <JournalItem
+            projectId="torchie"
+            chunkId="US143242"
+            chunkText="Incorrupte consequuntur mei quo argumentum tristique doming. homero senectus mei argumentum eirmod morbi."
+          />
+          <JournalItem
+            projectId="torchie"
+            chunkId="US157874"
+            chunkText="Pellentesque habitant morbi tristique senectus."
+          />
+          <JournalItem
+            projectId="torchie"
+            chunkId="US153345"
+            chunkText="Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id."
+          />
+          <JournalItem
+            projectId="torchie"
+            chunkId="US152224"
+            chunkText="nominati quo argumentum tristique doming. homero senectus mei Pellentesque."
+          />
+          <JournalItem
+            projectId="torchie"
+            chunkId="US154445"
+            chunkText="argumentum eirmod morbi Incorrupte intellegebat."
+          />
+          <JournalItem
+            projectId="torchie"
+            chunkId="US233556"
+            chunkText="Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id."
+          />
+          <JournalItem
+            projectId="torchie"
+            chunkId="US233453"
+            chunkText="Doming. homero senectus mei Pellentesque. Incorrupte consequuntur mei id."
+          />
         </Grid>
       </div>
     );
