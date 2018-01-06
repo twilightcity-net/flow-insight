63c247c297edb12c4afb8ce824338a9c005f805b
Kara Marie Rawson <rawsonkara@gmail.com>
2018-01-06 00:16:14 -0500

    added spirit content and animated content in sidebar panel

--- src/components/ConsoleSidebarPanel.js
+++ src/components/ConsoleSidebarPanel.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { RendererEvent } from "../RendererEventManager";
 import { RendererEventManagerHelper } from "../RendererEventManagerHelper";
-import { Segment } from "semantic-ui-react";
+import {
+  Icon,
+  Image,
+  Menu,
+  Progress,
+  Segment,
+  Transition
+} from "semantic-ui-react";
 
 //
 // this component is the tab panel wrapper for the console content
@@ -9,6 +16,12 @@ import { Segment } from "semantic-ui-react";
 export default class ConsoleSidebarPanel extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      activeItem: "spirit",
+      spiritVisible: true,
+      badgesVisible: false,
+      animationDelay: 500
+    };
     this.events = {
       sidebarPanel: new RendererEvent(
         RendererEventManagerHelper.Events.VIEW_CONSOLE_SIDEBAR_PANEL,
@@ -26,7 +39,7 @@ export default class ConsoleSidebarPanel extends Component {
     let heights = {
       rootBorder: 4,
       contentMargin: 8,
-      contentHeader: 47,
+      contentHeader: 34,
       bottomMenuHeight: 28
     };
 
@@ -39,8 +52,46 @@ export default class ConsoleSidebarPanel extends Component {
     );
   }
 
+  handleSpiritClick = (e, { name }) => {
+    this.setState({
+      activeItem: name,
+      spiritVisible: false,
+      badgesVisible: false
+    });
+    setTimeout(() => {
+      this.setState({
+        spiritVisible: true
+      });
+    }, this.state.animationDelay);
+  };
+
+  handleBadgesClick = (e, { name }) => {
+    this.setState({
+      activeItem: name,
+      spiritVisible: false,
+      badgesVisible: false
+    });
+    setTimeout(() => {
+      this.setState({
+        badgesVisible: true
+      });
+    }, this.state.animationDelay);
+  };
+
   /// renders the console sidebar panel of the console view
   render() {
+    const { activeItem } = this.state;
+    const spiritContent = (
+      <div className="spiritContent">
+        <Image centered src="./assets/images/spirit.png" />
+        <Progress size="small" percent={70} color="violet" inverted progress>
+          3466/28993 XP
+        </Progress>
+      </div>
+    );
+    const badgesContent = (
+      <div className="badgesContent">No Badges Earned :(</div>
+    );
     return (
       <div
         id="component"
@@ -51,9 +102,33 @@ export default class ConsoleSidebarPanel extends Component {
         }}
       >
         <Segment.Group>
-          <Segment inverted>Spirit</Segment>
+          <Menu size="mini" inverted pointing secondary>
+            <Menu.Item
+              name="spirit"
+              active={activeItem === "spirit"}
+              onClick={this.handleSpiritClick}
+            />
+            <Menu.Item
+              name="badges"
+              active={activeItem === "badges"}
+              onClick={this.handleBadgesClick}
+            />
+          </Menu>
           <Segment inverted style={{ height: this.calculateMenuHeight() }}>
-            Content
+            <Transition
+              visible={this.state.spiritVisible}
+              animation="fly right"
+              duration={this.state.animationDelay}
+            >
+              {spiritContent}
+            </Transition>
+            <Transition
+              visible={this.state.badgesVisible}
+              animation="fly right"
+              duration={this.state.animationDelay}
+            >
+              {badgesContent}
+            </Transition>
           </Segment>
         </Segment.Group>
       </div>
