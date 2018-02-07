d141ef872273aab5c28f3de775df37b58533edfd
Kara Marie Rawson <rawsonkara@gmail.com>
2018-02-07 09:09:15 -0500

    split tests into individual files, and simplified code

---
+++ test/api/ApiResourceAccountActivateTest.js
@@ -0,0 +1,106 @@
+const assert = require("assert");
+const chai = require("chai");
+const request = require("superagent");
+const Util = require("../UtilTest");
+const ActivationTokenDto = require("../../public/dto/ActivationTokenDto");
+const AccountActivationDto = require("../../public/dto/AccountActivationDto");
+
+//globals
+const port = process.env.PORT || 5000;
+const apiUrlLocal = "http://localhost:" + port;
+const apiUrlRemote = "http://htmflow.dreamscale.io";
+
+// TODO add tests for DataManager to be implemented
+describe("API Resource : /account ", function() {
+  let apiKey = "123e4567-e89b-12d3-a456-426655440000";
+  describe("POST /account/activate", function() {
+    let msg = "Your account has been successfully activated.";
+    let status = "VALID";
+    let email = "kara@dreamscale.io";
+    let key = "FASFD423fsfd32d2322d";
+    let token = "abcd12345678";
+    let dtoReq = new ActivationTokenDto({
+      activationToken: token
+    });
+    let dtoRes = new AccountActivationDto({
+      status: status,
+      message: msg,
+      email: email,
+      apiKey: key
+    });
+    let localDtoRes = null;
+    let remoteDtoRes = null;
+
+    it("should use dto 'ActivationTokenDto' for request", function(done) {
+      chai.should();
+      testDtoReq(dtoReq, token);
+      done();
+    });
+
+    it("should use dto 'AccountActivationDto' for response", function(done) {
+      chai.should();
+      testDtoRes(dtoRes, status, msg, email, key);
+      done();
+    });
+
+    it("should return 'AccountActivationDto' on local POST", function(done) {
+      chai.should();
+      request
+        .post(apiUrlLocal + "/account/activate")
+        .send(dtoReq)
+        .set("Content-Type", "application/json")
+        .end((err, res) => {
+          Util.checkRequestError(err);
+          localDtoRes = new AccountActivationDto(res.body);
+          testDtoRes(localDtoRes, status, msg, email, key);
+          done();
+        });
+    });
+
+    it("should return 'AccountActivationDto' on remote POST", function(done) {
+      chai.should();
+      request
+        .post(apiUrlRemote + "/account/activate")
+        .send(dtoReq)
+        .set("Content-Type", "application/json")
+        .end((err, res) => {
+          Util.checkRequestError(err);
+          remoteDtoRes = new AccountActivationDto(res.body);
+          testDtoRes(remoteDtoRes, status, msg, email, key);
+          done();
+        });
+    });
+
+    it("expect 'AccountActivationDto' to be equal", function(done) {
+      var expect = chai.expect;
+      expect(localDtoRes).to.eql(remoteDtoRes);
+      done();
+    });
+  });
+});
+
+function testDtoReq(dto, token) {
+  dto.should.have
+    .property("activationToken")
+    .with.be.a("string")
+    .with.equal(token);
+}
+
+function testDtoRes(dto, status, msg, email, key) {
+  dto.should.have
+    .property("status")
+    .with.be.a("string")
+    .with.equal(status);
+  dto.should.have
+    .property("message")
+    .with.be.a("string")
+    .with.equal(msg);
+  dto.should.have
+    .property("email")
+    .with.be.a("string")
+    .with.equal(email);
+  dto.should.have
+    .property("apiKey")
+    .with.be.a("string")
+    .with.equal(key);
+}
---
+++ test/api/ApiResourceAccountLoginTest.js
@@ -0,0 +1,85 @@
+const assert = require("assert");
+const chai = require("chai");
+const request = require("superagent");
+const Util = require("../UtilTest");
+const SimpleStatusDto = require("../../public/dto/SimpleStatusDto");
+
+//globals
+const port = process.env.PORT || 5000;
+const apiUrlLocal = "http://localhost:" + port;
+const apiUrlRemote = "http://htmflow.dreamscale.io";
+
+// TODO add tests for DataManager to be implemented
+describe("API Resource : /account/login ", function() {
+  let apiKey = "123e4567-e89b-12d3-a456-426655440000";
+
+  describe("POST /account/login", function() {
+    let msg = "Successfully logged in";
+    let status = "VALID";
+    let dtoRes = new SimpleStatusDto({
+      message: msg,
+      status: status
+    });
+    let localDtoRes = null;
+    let remoteDtoRes = null;
+
+    it("should use dto 'SimpleStatusDto' for response", function(done) {
+      chai.should();
+      testDtoRes(dtoRes, msg, status);
+      done();
+    });
+
+    it("should return 'SimpleStatusDto' on local POST", function(done) {
+      chai.should();
+      request
+        .post(apiUrlLocal + "/account/login")
+        .set("Content-Type", "application/json")
+        .set("X-API-Key", apiKey)
+        .end((err, res) => {
+          Util.checkRequestError(err);
+          localDtoRes = new SimpleStatusDto(res.body);
+          testDtoRes(localDtoRes, msg, status);
+          done();
+        });
+    });
+
+    //////////////////////////////////////////////////////////////
+    /// This fails now until the server returns back test data ///
+    //////////////////////////////////////////////////////////////
+
+    it("should return 'SimpleStatusDto' on remote POST", function(done) {
+      chai.should();
+      request
+        .post(apiUrlRemote + "/account/login")
+        .set("Content-Type", "application/json")
+        .set("X-API-Key", apiKey)
+        .end((err, res) => {
+          Util.checkRequestError(err);
+          remoteDtoRes = new SimpleStatusDto(res.body);
+          testDtoRes(remoteDtoRes, msg, status);
+          done();
+        });
+    });
+
+    //////////////////////////////////////////////////////////////
+    /// This fails now until the server returns back test data ///
+    //////////////////////////////////////////////////////////////
+
+    it("expect 'SimpleStatusDto' to be equal", function(done) {
+      var expect = chai.expect;
+      expect(localDtoRes).to.eql(remoteDtoRes);
+      done();
+    });
+  });
+});
+
+function testDtoRes(dto, msg, status) {
+  dto.should.have
+    .property("message")
+    .with.be.a("string")
+    .with.equal(msg);
+  dto.should.have
+    .property("status")
+    .with.be.a("string")
+    .with.equal(status);
+}
