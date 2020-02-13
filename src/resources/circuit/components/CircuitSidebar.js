14d810c46f46996daaf1f64a0e8d5533b5037816
ZoeDreams <kara@dreamscale.io>
2020-02-13 17:19:30 -0600

    increment version

--- src/resources/circuit/components/CircuitSidebar.js
+++ src/resources/circuit/components/CircuitSidebar.js
@@ -100,6 +100,42 @@ export default class CircuitSidebar extends Component {
             onClick={this.handleMenuClick}
           />
         </Menu>
+        {this.getCircuitSidebarMenuContent()}
+      </Segment>
+    );
+  }
+
+  /**
+   * renders the circuit sidebar menu content
+   * @returns {*}
+   */
+  getCircuitSidebarMenuContent() {
+    switch (this.state.activeMenuView) {
+      case CircuitSidebar.Views.OVERVIEW:
+        return this.getCircuitSidebarOverviewContent();
+      case CircuitSidebar.Views.PARTY:
+        return this.getCircuitSidebarPartyContent();
+      case CircuitSidebar.Views.CHEST:
+        return this.getCircuitSidebarChestContent();
+      default:
+        throw new Error(
+          "Unknown circuit sidebar menu type '" +
+            this.state.activeMenuView +
+            "'"
+        );
+    }
+  }
+
+  /**
+   * renders the circuit sidebar menu content for the overview section.
+   * This section contains a timer that counts up from start time, a title
+   * for the room, as well a a description along with an array of hashtag
+   * labels
+   * @returns {*}
+   */
+  getCircuitSidebarOverviewContent() {
+    return (
+      <div>
         <Label color="red" basic className="time">
           <Icon name="lightning" /> <span className="time"> 00:00:00:00</span>
         </Label>
@@ -155,6 +191,29 @@ export default class CircuitSidebar extends Component {
           <Label color="grey" size="tiny">
             Java
           </Label>
+        </Segment>
+      </div>
+    );
+  }
+
+  /**
+   * renders the circuit sidebar menu content for the party members
+   * whom have join this active circuits trouble shooting wtf session.
+   * @returns {*}
+   */
+  getCircuitSidebarPartyContent() {
+    return (
+      <div>
+        <Segment inverted className="title">
+          Party Members
+        </Segment>
+        <Segment inverted className="tags">
+          <Label color="grey" size="tiny">
+            JavaScript
+          </Label>
+          <Label color="grey" size="tiny">
+            Java
+          </Label>
           <Label color="grey" size="tiny">
             SaSS
           </Label>
@@ -185,8 +244,76 @@ export default class CircuitSidebar extends Component {
           <Label color="grey" size="tiny">
             Important
           </Label>
+          <Label color="grey" size="tiny">
+            JavaScript
+          </Label>
+          <Label color="grey" size="tiny">
+            Java
+          </Label>
         </Segment>
-      </Segment>
+      </div>
+    );
+  }
+
+  /**
+   * renders the circuit sidebar menu content for chest content. The
+   * chest content contains an array of linked resources. for example
+   * a snippet or screenshot. These items when clicked on will appear
+   * to the left of this panel and to the right of the chat panel within
+   * a resizable panel
+   * @returns {*}
+   */
+  getCircuitSidebarChestContent() {
+    return (
+      <div>
+        <Segment inverted className="title">
+          Chest Content Icon List
+        </Segment>
+        <Segment inverted className="tags">
+          <Label color="grey" size="tiny">
+            JavaScript
+          </Label>
+          <Label color="grey" size="tiny">
+            Java
+          </Label>
+          <Label color="grey" size="tiny">
+            SaSS
+          </Label>
+          <Label color="grey" size="tiny">
+            Gradle
+          </Label>
+          <Label color="grey" size="tiny">
+            GUI
+          </Label>
+          <Label color="grey" size="tiny">
+            DB
+          </Label>
+          <Label color="grey" size="tiny">
+            Technical
+          </Label>
+          <Label color="grey" size="tiny">
+            Game
+          </Label>
+          <Label color="grey" size="tiny">
+            Development
+          </Label>
+          <Label color="grey" size="tiny">
+            SomeRandom Tag
+          </Label>
+          <Label color="grey" size="tiny">
+            Tester
+          </Label>
+          <Label color="grey" size="tiny">
+            Important
+          </Label>
+          <Label color="grey" size="tiny">
+            JavaScript
+          </Label>
+          <Label color="grey" size="tiny">
+            Java
+          </Label>
+        </Segment>
+      </div>
     );
   }
 
@@ -238,6 +365,10 @@ export default class CircuitSidebar extends Component {
     );
   }
 
+  /**
+   * renders the circuit sidebar panel
+   * @returns {*}
+   */
   render() {
     return (
       <div id="component" className="circuitContentSidebar">
