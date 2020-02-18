baf1494ac56a04c2cf9118ed1d5814fb0736e982
ZoeDreams <kara@dreamscale.io>
2020-02-17 23:33:30 -0600

    working on our chat text editor

--- src/resources/circuit/components/ActiveCircuitChat.js
+++ src/resources/circuit/components/ActiveCircuitChat.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Segment } from "semantic-ui-react";
+import { Icon, Image, Input, Menu, Segment } from "semantic-ui-react";
 
 /**
  * This class is used to render the active circuit feed in the console view
@@ -11,7 +11,30 @@ export default class ActiveCircuitFeed extends Component {
    */
   constructor(props) {
     super(props);
+    this.imageEmojiSrc = "./assets/images/emoji_cool.png";
   }
+
+  focusChat = () => {
+    document
+      .getElementById("activeCircuitChatContainer")
+      .classList.add("focused");
+  };
+
+  blurChat = () => {
+    document
+      .getElementById("activeCircuitChatContainer")
+      .classList.remove("focused");
+  };
+
+  handleClickControls = (e, { name }) => {
+    console.log("clicked on -> " + name);
+  };
+
+  handleOnChangeChatInput = (e, { value }) => {
+    console.log(value);
+    let textEl = document.getElementById("activeCircuitChatText");
+    textEl.innerHTML += value;
+  };
 
   /**
    * renders the active circuit chat panel for the feed
@@ -20,7 +43,63 @@ export default class ActiveCircuitFeed extends Component {
   render() {
     return (
       <div id="component" className="activeCircuitChat">
-        <Segment inverted>Chat</Segment>
+        <Segment inverted>
+          <Menu className="controls" inverted icon compact borderless fluid>
+            <Menu.Item name="font-color" onClick={this.handleClickControls}>
+              <Icon name="font" />
+            </Menu.Item>
+            <Menu.Item name="font-bgColor" onClick={this.handleClickControls}>
+              <Icon bordered inverted color="violet" name="font" />
+            </Menu.Item>
+            <Menu.Item name="font-smaller" onClick={this.handleClickControls}>
+              <Icon.Group>
+                <Icon name="font" />
+                <Icon name="angle double down" />
+              </Icon.Group>
+            </Menu.Item>
+            <Menu.Item name="font-medium" onClick={this.handleClickControls}>
+              <Icon name="font" />
+            </Menu.Item>
+            <Menu.Item name="font-larger" onClick={this.handleClickControls}>
+              <Icon.Group>
+                <Icon name="font" />
+                <Icon name="angle double up" />
+              </Icon.Group>
+            </Menu.Item>
+            <Menu.Item name="font-bold" onClick={this.handleClickControls}>
+              <Icon name="bold" />
+            </Menu.Item>
+            <Menu.Item name="font-italic" onClick={this.handleClickControls}>
+              <Icon name="italic" />
+            </Menu.Item>
+            <Menu.Item name="font-underline" onClick={this.handleClickControls}>
+              <Icon name="underline" />
+            </Menu.Item>
+            <Menu.Item name="font-link" onClick={this.handleClickControls}>
+              link
+            </Menu.Item>
+            <Menu.Item name="emoji" onClick={this.handleClickControls}>
+              <Image src={this.imageEmojiSrc} verticalAlign="top" />
+            </Menu.Item>
+          </Menu>
+          <div id="activeCircuitChatContainer" className="text-formatted">
+            <span id="activeCircuitChatText" className="chat-text" />
+            <span id="activeCircuitChatCursor" className="chat-cursor">
+              |
+            </span>
+            <span>
+              <Input
+                id="activeCircuitChatInput"
+                className="chat-input"
+                transparent
+                value=""
+                onFocus={this.focusChat}
+                onBlur={this.blurChat}
+                onChange={this.handleOnChangeChatInput}
+              />
+            </span>
+          </div>
+        </Segment>
       </div>
     );
   }
