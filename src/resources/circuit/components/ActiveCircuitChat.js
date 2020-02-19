9138425bd3b48d1309a022aa49c3f3079e8c1e5e
ZoeDreams <kara@dreamscale.io>
2020-02-19 17:36:22 -0600

    code clean up

--- src/resources/circuit/components/ActiveCircuitChat.js
+++ src/resources/circuit/components/ActiveCircuitChat.js
@@ -1,5 +1,5 @@
-import React, {Component, createRef} from "react";
-import {Icon, Image, Input, Menu, Segment} from "semantic-ui-react";
+import React, { Component, createRef } from "react";
+import { Icon, Image, Input, Menu, Segment } from "semantic-ui-react";
 
 /**
  * This class is used to render the active circuit feed in the console view
@@ -15,6 +15,8 @@ export default class ActiveCircuitChat extends Component {
     this.imageEmojiSrc = "./assets/images/emoji_cool.png";
     this.chatInputRef = createRef();
     this.keyArray = [];
+    this.isFocused = false;
+    this.isMouseDown = false;
   }
 
   getChatTextEl = () => {
@@ -22,15 +24,19 @@ export default class ActiveCircuitChat extends Component {
   };
 
   focusChat = () => {
+    this.isFocused = true;
     document
-    .getElementById("activeCircuitChatContainer")
-    .classList.add("focused");
+      .getElementById("activeCircuitChatContainer")
+      .classList.add("focused");
   };
 
   blurChat = () => {
-    document
-    .getElementById("activeCircuitChatContainer")
-    .classList.remove("focused");
+    if (this.isFocused) {
+      this.isFocused = false;
+      document
+        .getElementById("activeCircuitChatContainer")
+        .classList.remove("focused");
+    }
   };
 
   addCharToText = (keyCode, key) => {
@@ -45,34 +51,55 @@ export default class ActiveCircuitChat extends Component {
     el.innerText = el.innerText.slice(0, -1);
   };
 
-  isValidKey = kc => {
-    return kc > 31 &&
-      kc !== 91 &&
-      kc !== 93 &&
-      kc !== 45 &&
-      kc !== 35 &&
-      kc !== 36 &&
-      kc !== 33 &&
-      kc !== 34 &&
-      kc !== 37 &&
-      kc !== 39 &&
-      kc !== 38 &&
-      kc !== 40 &&
-      kc !== 124 &&
-      (kc < 112 ||
-      kc  > 124);
+  isValidKey = key => {
+    return (
+      key > 31 &&
+      key !== 91 &&
+      key !== 93 &&
+      key !== 45 &&
+      key !== 35 &&
+      key !== 36 &&
+      key !== 33 &&
+      key !== 34 &&
+      key !== 37 &&
+      key !== 39 &&
+      key !== 38 &&
+      key !== 40 &&
+      key !== 124 &&
+      (key < 112 || key > 124)
+    );
   };
 
   handleEnterKey = () => {
     console.log(this.name + " post to server and clear box");
   };
 
-  handleClickControls = (e, {name}) => {
+  handleClickControls = (e, { name }) => {
     console.log(this.name + " clicked on -> " + name);
   };
 
-  handleOnClickChatContainer = () => {
-    this.chatInputRef.current.focus();
+  handleOnClickChat = () => {
+    if (!this.isMouseDown) {
+      this.chatInputRef.current.focus();
+    }
+  };
+
+  handleOnMouseEnter = () => {
+    this.isSelecting = true;
+  };
+
+  handleOnMouseLeave = () => {
+    this.isSelecting = false;
+  };
+
+  handleOnMouseDown = () => {
+    console.log("down");
+    this.isMouseDown = true;
+  };
+
+  handleOnMouseUp = () => {
+    console.log("up");
+    this.isMouseDown = false;
   };
 
   handleOnKeyDownChatInput = e => {
@@ -94,6 +121,14 @@ export default class ActiveCircuitChat extends Component {
     }
   };
 
+  getTextCursor() {
+    return (
+      <span id="activeCircuitChatCursor" className="chat-cursor">
+        |
+      </span>
+    );
+  }
+
   /**
    * renders the active circuit chat panel for the feed
    * @returns {*}
@@ -102,53 +137,55 @@ export default class ActiveCircuitChat extends Component {
     return (
       <div id="component" className="activeCircuitChat">
         <Segment inverted>
-          <Menu className="controls" inverted icon compact borderless fluid>
+          <Menu className="controls" inverted icon compact borderless>
             <Menu.Item name="font-color" onClick={this.handleClickControls}>
-              <Icon name="font"/>
+              <Icon name="font" />
             </Menu.Item>
             <Menu.Item name="font-bgColor" onClick={this.handleClickControls}>
-              <Icon bordered inverted color="violet" name="font"/>
+              <Icon bordered inverted color="violet" name="font" />
             </Menu.Item>
             <Menu.Item name="font-smaller" onClick={this.handleClickControls}>
               <Icon.Group>
-                <Icon name="font"/>
-                <Icon name="angle double down"/>
+                <Icon name="font" />
+                <Icon name="angle double down" />
               </Icon.Group>
             </Menu.Item>
             <Menu.Item name="font-medium" onClick={this.handleClickControls}>
-              <Icon name="font"/>
+              <Icon name="font" />
             </Menu.Item>
             <Menu.Item name="font-larger" onClick={this.handleClickControls}>
               <Icon.Group>
-                <Icon name="font"/>
-                <Icon name="angle double up"/>
+                <Icon name="font" />
+                <Icon name="angle double up" />
               </Icon.Group>
             </Menu.Item>
             <Menu.Item name="font-bold" onClick={this.handleClickControls}>
-              <Icon name="bold"/>
+              <Icon name="bold" />
             </Menu.Item>
             <Menu.Item name="font-italic" onClick={this.handleClickControls}>
-              <Icon name="italic"/>
+              <Icon name="italic" />
             </Menu.Item>
             <Menu.Item name="font-underline" onClick={this.handleClickControls}>
-              <Icon name="underline"/>
+              <Icon name="underline" />
             </Menu.Item>
             <Menu.Item name="font-link" onClick={this.handleClickControls}>
               link
             </Menu.Item>
             <Menu.Item name="emoji" onClick={this.handleClickControls}>
-              <Image src={this.imageEmojiSrc} verticalAlign="top"/>
+              <Image src={this.imageEmojiSrc} verticalAlign="top" />
             </Menu.Item>
           </Menu>
           <div
             id="activeCircuitChatContainer"
             className="text-formatted"
-            onClick={this.handleOnClickChatContainer}
+            onClick={this.handleOnClickChat}
+            onMouseEnter={this.handleOnMouseEnter}
+            onMouseLeave={this.handleOnMouseLeave}
+            onMouseDown={this.handleOnMouseDown}
+            onMouseUp={this.handleOnMouseUp}
           >
-            <span id="activeCircuitChatText" className="chat-text"/>
-            <span id="activeCircuitChatCursor" className="chat-cursor">
-              |
-            </span>
+            <span id="activeCircuitChatText" className="chat-text" />
+            {this.getTextCursor()}
             <span>
               <Input
                 id="activeCircuitChatInput"
