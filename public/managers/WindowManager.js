b8acde3760ce481e972582850a2ab3010ed14746
ZoeDreams <kara@dreamscale.io>
2020-02-19 19:43:51 -0600

    these should be on.

--- public/managers/WindowManager.js
+++ public/managers/WindowManager.js
@@ -76,7 +76,7 @@ class WindowManager {
     global.App.WindowManager.lastBlurWindowName = arg.sender.name;
     global.App.ShortcutManager.deactivateWindowShortcuts(arg.sender);
     if (arg.sender.name === WindowManagerHelper.WindowNames.CONSOLE) {
-      // this.handleHideConsoleEvent(1);
+      this.handleHideConsoleEvent(1);
     }
   }
 
--- public/windows/ConsoleWindow.js
+++ public/windows/ConsoleWindow.js
@@ -41,7 +41,7 @@ module.exports = class ConsoleWindow {
     });
     this.window.name = this.name;
     this.window.setMenu(null);
-    // this.window.setAlwaysOnTop(true, "torn-off-menu");
+    this.window.setAlwaysOnTop(true, "torn-off-menu");
     this.window.on("ready-to-show", () => this.onReadyToShowCb());
     this.events = {
       ready: EventFactory.createEvent(
