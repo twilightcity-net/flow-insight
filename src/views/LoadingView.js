37b7a13dd726487ba0176d51a409c1b7dc673177
ZoeDreams <kara@dreamscale.io>
2020-05-05 21:06:23 -0500

    updated version

--- src/views/LoadingView.js
+++ src/views/LoadingView.js
@@ -36,7 +36,7 @@ export default class LoadingView extends Component {
       label: "Populating cats and synthesizers"
     };
     this.state = {
-      appVersion: "0.4.7",
+      appVersion: "0.4.8",
       visible: true,
       header: this.header,
       progress: this.progress,
