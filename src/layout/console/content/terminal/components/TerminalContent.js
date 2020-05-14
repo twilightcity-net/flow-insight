bf9c4dbe485f7cc026494eba8864a036f2c1dd03
ZoeDreams <kara@dreamscale.io>
2020-05-14 00:45:26 -0500

    update the version on this file

--- src/layout/console/content/terminal/components/TerminalContent.js
+++ src/layout/console/content/terminal/components/TerminalContent.js
@@ -36,7 +36,7 @@ export default class TerminalContent extends Component {
           "Display version information for this shell",
         usage: "version",
         fn: () => {
-          return "v0.4.1 Higgly Heights";
+          return "v0.4.8 Flipin' Fools";
         }
       },
       whoami: {
