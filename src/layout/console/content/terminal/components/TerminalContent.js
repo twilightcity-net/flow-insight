fabd53d6e8179e2d0b682778facf22446c2a1242
ZoeDreams <kara@dreamscale.io>
2020-06-29 19:32:27 -0500

    BUMPED VERSION

--- src/layout/console/content/terminal/components/TerminalContent.js
+++ src/layout/console/content/terminal/components/TerminalContent.js
@@ -36,7 +36,7 @@ export default class TerminalContent extends Component {
           "Display version information for this shell",
         usage: "version",
         fn: () => {
-          return "v.Psyki_0.5.0";
+          return "v.Psyki_0.5.1";
         }
       },
       whoami: {
