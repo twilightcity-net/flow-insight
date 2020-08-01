a25d2b06b3bdd13b207f0fd5e963a17eccdb8752
ZoeDreams <kara@dreamscale.io>
2020-07-31 22:09:36 -0500

    increment version to v0.5.2

--- src/views/LoadingView.js
+++ src/views/LoadingView.js
@@ -36,7 +36,7 @@ export default class LoadingView extends Component {
       label: "Populating cats and synthesizers"
     };
     this.state = {
-      appVersion: "Psyki_0.5.1",
+      appVersion: "Psyki_0.5.2",
       visible: true,
       header: this.header,
       progress: this.progress,
