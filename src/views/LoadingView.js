861e347c02d00ca7695fe77fe2db3433d946d6a1
Arty Starr <arty@twilightcity.net>
2022-03-14 09:04:18 -0700

    Update version numbers for release

--- src/views/LoadingView.js
+++ src/views/LoadingView.js
@@ -36,7 +36,7 @@ export default class LoadingView extends Component {
       label: "Populating cats and synthesizers",
     };
     this.state = {
-      appVersion: "0.5.15",
+      appVersion: "0.5.16",
       visible: true,
       header: this.header,
       progress: this.progress,
