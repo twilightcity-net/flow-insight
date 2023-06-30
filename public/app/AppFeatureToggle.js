c55c27d86aa96be2e8a2e14c019edc6f0c145ad3
Arty Starr <arty@twilightcity.net>
2023-06-30 16:13:51 -0700

    Increment version without 0 in the increment

--- public/app/AppFeatureToggle.js
+++ public/app/AppFeatureToggle.js
@@ -13,7 +13,7 @@ module.exports = class AppFeatureToggle {
   static isStatusBarEnabled = false;
 
   static appName = "FlowInsight";
-  static version = "0.7.02"
+  static version = "0.7.12"
 
   static isFlowInsightApp() {
     return !AppFeatureToggle.isMoovieApp;
--- src/layout/shared/FeatureToggle.js
+++ src/layout/shared/FeatureToggle.js
@@ -19,7 +19,7 @@ export default class FeatureToggle  {
   static isStatusBarEnabled = false;
 
   static appName = "FlowInsight";
-  static version = "0.7.02"
+  static version = "0.7.12"
 
   static isFlowInsightApp() {
     return !FeatureToggle.isMoovieApp;
