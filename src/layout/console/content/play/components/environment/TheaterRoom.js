84157c528711d1b88790b28bc0d7d91f52b0c5f3
Arty Starr <arty@twilightcity.net>
2023-07-30 19:42:41 -0700

    Remove @ from theater name tips

--- src/layout/console/content/play/components/environment/TheaterRoom.js
+++ src/layout/console/content/play/components/environment/TheaterRoom.js
@@ -467,7 +467,7 @@ export default class TheaterRoom extends Environment {
   drawFervieTooltip(p5, fervieInSeat) {
     let name = fervieInSeat.fervieName;
     if (!name) {
-      name = "@"+fervieInSeat.username;
+      name = fervieInSeat.username;
     }
     p5.fill('rgba(0,0,0,0.7)');
     p5.rect(fervieInSeat.x - 45, fervieInSeat.y - 100, 100, 30, 5);
