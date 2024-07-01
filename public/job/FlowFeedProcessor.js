e8e400efeb5c8ff1d0052388f238f2845ac8cf56
Arty Starr <arty@twilightcity.net>
2024-07-01 14:47:25 -0700

    Add a delay to keep excess of data from spamming the server

--- public/job/FlowFeedProcessor.js
+++ public/job/FlowFeedProcessor.js
@@ -447,6 +447,40 @@ module.exports = class FlowFeedProcessor {
   }
 
 
+  /**
+   * Publish all the batches in the batch folder to the server,
+   * Not sending more than 5 at a time before sleeping
+   * @param pluginId
+   */
+  publishAllBatchesWithDelays(pluginId) {
+    const queueFolder = this.getQueueFolder(pluginId);
+
+    Util.createFolderIfDoesntExist(queueFolder, () => {
+      this.findAllFilesInPublishQueue(pluginId, (batchFileList) => {
+        batchFileList.forEach((file, index) => {
+          const filePath = path.join(queueFolder, file);
+
+          setTimeout(() =>  {
+            log.info(this.name + " Publishing "+filePath);
+            const flowBatchDto = this.createEmptyFlowBatch();
+
+            this.flowFileReader.asyncProcessFile(filePath, {}, (data, lineType, json) => {
+              this.addToFlowBatch(flowBatchDto, lineType, json);
+            }).then((successfulParse) => {
+                if (successfulParse) {
+                  this.publishBatch(pluginId, flowBatchDto, filePath, file);
+                } else {
+                  this.handleParseFailure(pluginId, filePath, file);
+                }
+              }
+            );
+          }, index * 2000);
+
+        });
+      });
+    });
+  }
+
   addToFlowBatch = (flowBatchObj, lineType, json) => {
     switch (lineType) {
       case FlowFeedProcessor.EditorActivity:
